794a183d17a5d27eff928716e3ad3596e2eb9265
Santiago Arango <santiago.varon@gmail.com>
2014-11-17 06:56:27 -0500

    mejoras concecionario

--- public/javascripts/app_concesionario.js
+++ public/javascripts/app_concesionario.js
@@ -15,7 +15,7 @@ app.directive('validarOcupado', function($compile) {
 
   var imageTemplate = '<div class="entry-photo"><h2>&nbsp;</h2><div class="entry-img"><span><a href="{{rootDirectory}}{{content.data}}"><img ng-src="{{rootDirectory}}{{content.data}}" alt="entry photo"></a></span></div><div class="entry-text"><div class="entry-title">{{content.title}}</div><div class="entry-copy">{{content.description}}</div></div></div>';
   var btn = '<div class="button">Aceptar hora</div>';
-  var concesionario = '<p>Cita aceptada por {{content.concecionario.name}}</p>';
+  var concesionario = '<p>Cita aceptada por {{content.concesionario.name}}</p>';
 
 
   var linker = function(scope, element, attrs) {
