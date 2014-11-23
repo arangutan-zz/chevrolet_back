a7fa1831d987dbee040dcacb288184b0d1b1536e
santiago varon <santiago.varon@gmail.com>
2014-11-23 11:34:32 -0500

    se agregaron camiones

--- public/javascripts/app_tv.js
+++ public/javascripts/app_tv.js
@@ -12,7 +12,8 @@ window.onload = function() {
                             '<div class="row">'+
                               '<div class="columns large-7">'+
                                 '<h5>VENDEDOR:</h5>'+
-                                '<h3>'+data.vendedor.name+'</h3>'+
+                               <!-- '<h3>'+data.vendedor.name+'</h3>'+ -->
+				'<h3>Vendedor</h3>'+
                               '</div>'+
                               '<div class="columns large-5">'+
                                 '<h5>COMPRADOR:</h5>'+
@@ -20,10 +21,10 @@ window.onload = function() {
                               '</div>'+
                             '</div>'+
                             '<div class="row">'+
-                              '<div class="columns large-7">'+
-                                '<h5>CONCESIONARIO:</h5>'+
-                                '<h3>'+data.turno.name+'</h3>'+
-                              '</div>'+
+                           
+                             
+                               
+                              
                               '<div class="columns large-5">'+
                                 '<h5>DESTINO:</h5>'+
                                 '<h3>'+data.car+'</h3>'+
